a0ffe172cc2fdb871cf2d282b7e0f90d66f098a2
gsaiteja001 <saiteja244139@gmail.com>
2025-04-14 19:48:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./routes/index');
 
 const app = express();
 const PORT = 3000;
